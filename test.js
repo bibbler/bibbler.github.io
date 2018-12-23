1fccf8d7cc24b1665d723b0bb15a0da987122866
bibbler <airbobs@ymail.com>
2018-12-22 20:37:02 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 function init() {
  Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1ZCLS7KCRRsVRjqAIrr_nIVSSvT9uTkpdHUrW4ZMI_qY/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
+ callback: showInfo
  simpleSheet: true } )
 }
+
+function showInfo(data, tabletop) {
+console.log(data);
+ 
+for (var i = 0; i < data.length; i++) {
+$(‘.post’).append(
+‘<div class=”article”>’ +
+‘<div class=”text”>’ +
+‘<h1>’ + data[i].date + ‘</h1>’+
+data[i].url +
+‘</div>‘
+‘</div>’);
+ 
+ }
+ }
+
 window.addEventListener(‘DOMContentLoaded’, init)
 
